db069d0bc90b79e57da43050546838790fc21884
CR-97 <c_richard97@outlook.com>
2018-11-22 14:16:21 +0800

    minor change

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ var Users = require('./route/Users');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //Connect to mongoose db
 mongoose.connect(db,{ useNewUrlParser: true })
@@ -110,7 +118,8 @@ app.get('/getMatches', (req, res) => {
   })
 });
 
+const port = process.env.PORT || 5000;
 
-app.listen(5000, ()=>{
-  console.log('Server started on port 5000');
+app.listen(port, ()=>{
+  console.log(`Server started on port ${port}`);
 });
